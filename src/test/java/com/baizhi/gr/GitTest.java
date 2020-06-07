449a45da906956b8785b81a5cd9662accd0b08aa
gaor <gr18237343896@163.com>
2020-06-07 19:24:25 +0800

    更新git测试类,添加@param注释

--- src/test/java/com/baizhi/gr/GitTest.java
+++ src/test/java/com/baizhi/gr/GitTest.java
@@ -5,6 +5,9 @@ package com.baizhi.gr;
  * @Date: 2020/6/7 18:26
  */
 public class GitTest {
+    /**
+     * @param
+     * */
     public void testGit(){
         System.out.println("测试下Git");
     }
